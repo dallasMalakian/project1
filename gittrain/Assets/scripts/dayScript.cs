9c62edc1c2aad54159441530d52ab6daf2108473
dallasMalakian <lpsdg2003@gmail.com>
2019-08-01 14:28:36 +0300

    Scene1

---
+++ gittrain/Assets/scripts/dayScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dayScript : MonoBehaviour
+{
+    public float speed = 0;
+    void Start()
+    {
+       
+    }
+
+    
+    void Update()
+    {
+        gameObject.transform.Rotate(speed, 0, 0);
+    }
+}
